ae318f6689d8a3bb8551521630e9070901e5fb2f
Not-A-Sith-Lord <arieldeschapell@gmail.com>
2017-09-25 19:11:11 -0400

    setting up for blockchain integration

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,32 @@ const router  = express.Router();
 const download = require('download-git-repo');
 const unzip = require('unzip');
 const fs = require('fs');
-const LastTweet = require('../models/lastTweet.js');
 
 
-//global variable, to save which is the last tweet we've published to blockchain
+
+
+
 
 /* GET home page. */
-
+//***To be converted to business logic that runs every hour***
 router.get('/', (req, res, next) => {
 
-//To be converted to business logic that runs every hour
-  //Download repo of trump tweets
+  //This will hold the new tweets yet to be encoded
+  const memArray = [];
+
+  //List of last tweets which were added to memArray
+  const LastTweet = require('../models/lastTweet.js');
+
+
+  //Download updated repo of all trump tweets
   download('bpb27/trump_tweet_data_archive', 'tweetResults', (err) => {
     console.log(err ? 'Error' : 'Success');
 
   //Take 2017 json file from results and unzip
   fs.createReadStream('tweetResults/condensed_2017.json.zip').pipe(unzip.Extract({ path: 'parsedTweets' }));
+  //unzipped 2017 tweets
   const parsedTweets = require('../parsedTweets/condensed_2017.json');
 
-  // console.log(parsedTweets);
 
   //Get id of last encoded tweet for conditional to exit loop
   LastTweet.find((err, returnedLastTweets) => {
@@ -29,23 +36,21 @@ router.get('/', (req, res, next) => {
       //Need to fail gracefully and retry later
       return next(err);
       }
-      //get last id not first
-
+      //Twitter ID of last tweet we added to memArray
       const lastTweetId = returnedLastTweets[returnedLastTweets.length - 1].tweet_id_str;
-
-
 
 
   // console.log("LAST TWEET ID IS------------" + lastTweet + "-------------------------");
 
+  //To stop adding
+  let doneLooping = false;
   //Loop through tweets, push wanted info into new [{},{}] to publish blockchain
-
   for (const [index, tweet] of parsedTweets.entries()) {
 
     //if the tweet has already been encoded
-    if (tweet.id_str === lastTweetId){
-
-      newLastTweetId = parsedTweets[index - 1].id_str;
+    if (tweet.id_str === "912301271817838593"){
+      doneLooping = true;
+      newLastTweetId = parsedTweets[0].id_str;
 
       const newLastTweet = new LastTweet({tweet_id_str: newLastTweetId});
       console.log("THIS IS THE NEW LAST TWEET:" + newLastTweet);
@@ -57,14 +62,26 @@ router.get('/', (req, res, next) => {
 
       });
 
-      return;
+
     }
     //End IF
-    console.log("-----------------");
 
-    //These are tweets that will be pushed to encode prep
-    console.log(tweet.text);
+
+
+
+
+    // ELSE These are tweets that will be pushed to encode prep
+    if (!doneLooping) {
+    memArray.push({
+      text: tweet.text,
+      createdAt: tweet.created_at
+    })
+  }
+
   };
+console.log("MEM ARRAY: " + memArray);
+  //End of looping, memArray now populated and ready to encode
+
 
 
 
