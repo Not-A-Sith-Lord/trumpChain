a73672039716fa87eb6486e32940ff96c26b9d22
Oleh Kolinko <oleh.kolinko@gmail.com>
2017-10-14 22:49:45 -0400

    changed saving to file from json to txt

--- appFunctions/saveToFile.js
+++ appFunctions/saveToFile.js
@@ -1,33 +1,17 @@
 const fs              = require('fs');
 const updateJsonFile  = require('update-json-file');
 
-module.exports = function saveToFile( newTweets ){
-  //newTweets is array of new tweets
+module.exports =  function saveToFile( newTweets ){
 
-  //Generating filePath:
-  const year = new Date().getFullYear(); //Current year
-  const month = new Date().getMonth() + 1; //Current month
-  const filePath = `./tweetResults/tweets-${year}-${month}.json`; //1 file per month
+  newTweets.forEach(tweet => {
 
+    //Generating filePath:
+    const year = new Date(tweet.originalContent.created_at).getFullYear(); //Current year
+    const month = new Date(tweet.originalContent.created_at).getMonth() + 1; //Current month
+    const filePath = `./tweetResults/tweets-${year}-${month}.txt`; //1 file per month
 
-  //Create new file if it's not exist
-  if (!fs.existsSync(filePath)) {
-    const startingJSON =  '[]' ; //Simplest JSON for holding array
+    fs.appendFileSync(filePath,  JSON.stringify(tweet, null, 4) + '\n');
+    console.log(`Added new tweet to file = '${filePath}', tweet text = ${tweet.originalContent.text}`);
 
-    fs.writeFileSync(filePath, startingJSON , 'utf8', (err) => {
-      if (err) return console.log(err);
-      console.log(`The file was succesfully created! filePath = ${filePath}`);
-    });
-  }
-
-  //Add all new tweets to JSON file:
-  updateJsonFile(filePath, (data) => {
-    console.log(`Updating file. filePath = '${filePath}'`);
-
-    newTweets = newTweets.filter( tweet => !data.includes(tweet));//Filter dubplicates if there are any
-
-    //Combine old data(in file) with new data(new tweets):
-    data = [ ...data, ...newTweets ];
-    return data
-  });
+  })
 }
